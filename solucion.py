678af605cd6fb6b52036ae779ec3a760f82a48de
ALEXA MORALES PEREZ VARGAS <amoral98@itam.mx>
2023-11-28 17:52:35 -0600

    graficas actualizadas

--- solucion.py
+++ solucion.py
@@ -5,11 +5,11 @@ import matplotlib.pyplot as plt
 
 # Lectura de los archivos.
 dfDatos = pd.read_csv(
-    "C:\\Users\\alexa\\Documents\\Alexa\\Escuela\\ITAM\\Desarrollo de Aplicaciones Informáticas\\Proyecto2\\P_Data_Extract_From_Health_Nutrition_and_Population_Statistics\\361d8e4b-91be-4bc2-bdca-fbbf19126345_Data.csv",
+    "C:\\Users\\alexa\\Documents\\Alexa\\Escuela\\ITAM\\Desarrollo de Aplicaciones Informáticas\\Proyecto2\\Analisis_Paises_Indicadores\\P_Data_Extract_From_Health_Nutrition_and_Population_Statistics\\361d8e4b-91be-4bc2-bdca-fbbf19126345_Data.csv",
     encoding="Latin-1",
 )
 dfDescr = pd.read_csv(
-    "C:\\Users\\alexa\\Documents\\Alexa\\Escuela\\ITAM\\Desarrollo de Aplicaciones Informáticas\\Proyecto2\\P_Data_Extract_From_Health_Nutrition_and_Population_Statistics\\361d8e4b-91be-4bc2-bdca-fbbf19126345_Series - Metadata.csv",
+    "\\Users\\alexa\\Documents\\Alexa\\Escuela\\ITAM\\Desarrollo de Aplicaciones Informáticas\\Proyecto2\\Analisis_Paises_Indicadores\\P_Data_Extract_From_Health_Nutrition_and_Population_Statistics\\361d8e4b-91be-4bc2-bdca-fbbf19126345_Series - Metadata.csv",
     encoding="Latin-1",
 )
 
@@ -19,17 +19,19 @@ dfDescr = pd.read_csv(
 
 paises = ["United States", "China", "Mexico", "Guatemala", "Germany"]
 indicadores = [
-    "SP.POP.TOTL",
-    "SP.POP.TOTL.MA.IN",
-    "SP.POP.TOTL.FE.IN",
-    "SP.DYN.CDRT.IN",
-    "SH.XPD.PVTD.CH.ZS",
-    "SH.XPD.GHED.PC.CD",
-    "SH.STA.SUIC.P5",
-    "SP.POP.80UP.FE.5Y",
-    "SP.POP.80UP.MA.5Y",
+    "Population, total",
+    "Population, male",
+    "Population, female",
+    "Death rate, crude (per 1,000 people)",
+    "Domestic private health expenditure (% of current health expenditure)",
+    "Domestic general government health expenditure per capita (current US$)",
+    "Suicide mortality rate (per 100,000 population)",
+    "Population ages 80 and older, female (% of female population)",
+    "Population ages 80 and above, male (% of male population)",
 ]
-years = [
+
+# Años
+nomcolumn = [
     "2000",
     "2001",
     "2002",
@@ -51,14 +53,50 @@ years = [
     "2018",
     "2019",
 ]
-
 # Filtrado de los datos.
 dfDatosFil = dfDatos[
-    (dfDatos["Country Name"].isin(paises)) & (dfDatos["Series Code"].isin(indicadores))
+    (dfDatos["Country Name"].isin(paises)) & (dfDatos["Series Name"].isin(indicadores))
 ]
 dfDescrFil = dfDescr[
-    (dfDescr["Country Name"].isin(paises)) & (dfDescr["Series Code"].isin(indicadores))
+    (dfDescr["Country Name"].isin(paises)) & (dfDescr["Series Name"].isin(indicadores))
 ]
+
+# 1) Gráficas por indicador por país
+for pais in paises:
+    for indicador in indicadores:
+        datos_pais = pd.to_numeric(
+            dfDatosFil[
+                (dfDatosFil["Country Name"] == pais)
+                & (dfDatosFil["Series Name"] == indicador)
+            ]
+            .iloc[:, 44:64]
+            .values.flatten()
+        )
+        plt.figure(figsize=(12, 8))
+        plt.title(f"{indicador} {pais}")
+        plt.plot(nomcolumn, datos_pais)
+        plt.xlabel("Año")  # etiqueta del eje x.
+        plt.ylabel("Valor")  # etiqueta del eje y.
+        plt.grid()  # agregar cuadrícula.
+        plt.show()
+
+# 2) Maxímo y mínimo de cada indicador y país
+for pais in paises:
+    print("País :", pais)
+    for indicador in indicadores:
+        datos_pais_nump = np.array(
+            dfDatosFil[
+                (dfDatosFil["Country Name"] == pais)
+                & (dfDatosFil["Series Name"] == indicador)
+            ]
+            .iloc[:, 44:64]
+            .values.flatten()
+        )
+        print("Indicador :", indicador)
+        print("Mínimo :", datos_pais_nump.min())
+        print("Máximo :", datos_pais_nump.max())
+        print()
+
 
 # 3) Indicadores en una única gráfica por país.
 for pais in paises:
@@ -67,20 +105,34 @@ for pais in paises:
 
     plt.figure(figsize=(12, 8))  # tamaño de la gráfica.
     plt.title(f"Indicadores {pais}")  # título de la gráfica.
+    
+    poblacionTotal = pd.to_numeric(datos_pais[datos_pais["Series Name"] == "Population, total"].iloc[:,44:64].values.flatten())
 
     for indicador in indicadores:
-        datos_indicador = (
-            datos_pais[datos_pais["Series Code"] == indicador]
-            .iloc[:, 44:64]
-            .values.flatten()
+        datos_indicador = pd.to_numeric(
+            (
+                datos_pais[datos_pais["Series Name"] == indicador]
+                .iloc[:, 44:64]
+                .values.flatten()
+            )
         )  # filtrar por indicador.
-        descr_indicador = descr_pais[descr_pais["Series Code"] == indicador][
-            "Series Code"
+        descr_indicador = descr_pais[descr_pais["Series Name"] == indicador][
+            "Series Name"
         ].values[
             0
         ]  # filtrar por indicador.
+            
+        datos_normalizados = datos_indicador / poblacionTotal # normalizar los datos.
+            
+        # 5) El país con el mínimo y el máximo de cada indicador junto con el año, a partir de las gráficas del punto 3.
+        min_value = datos_indicador.min()
+        max_value = datos_indicador.max()
+        year_min = nomcolumn[np.argmin(datos_indicador)]
+        year_max = nomcolumn[np.argmax(datos_indicador)]
 
-        plt.plot(years, datos_indicador)  # graficar los datos.
+        plt.plot(
+            nomcolumn, datos_normalizados, label=f"{indicador}"
+        )  # graficar los datos.
 
     plt.xlabel("Año")  # etiqueta del eje x.
     plt.ylabel("Valor")  # etiqueta del eje y.
@@ -88,28 +140,31 @@ for pais in paises:
     plt.legend(loc="upper left")  # agregar leyenda.
     plt.show()  # mostrar gráfica.
 
-
 # 4) Países en una gráfica por indicador.
 for indicador in indicadores:
     plt.figure(figsize=(12, 8))  # tamaño de la gráfica.
     plt.title(f"Países {indicador}")  # título de la gráfica.
 
     for pais in paises:
-        datosP_indicador = (
-            dfDatosFil[
-                (dfDatosFil["Country Name"] == pais)
-                & (dfDatosFil["Series Code"] == indicador)
-            ]
-            .iloc[:, 44:64]
-            .values.flatten()
+        datosP_indicador = pd.to_numeric(
+            (
+                dfDatosFil[
+                    (dfDatosFil["Country Name"] == pais)
+                    & (dfDatosFil["Series Name"] == indicador)
+                ]
+                .iloc[:, 44:64]
+                .values.flatten()
+            )
         )  # filtrar por indicador.
         descrP_indicador = dfDescrFil[
             (dfDescrFil["Country Name"] == pais)
-            & (dfDescrFil["Series Code"] == indicador)
-        ]["Series Code"].values[
+            & (dfDescrFil["Series Name"] == indicador)
+        ]["Series Name"].values[
             0
         ]  # filtrar por indicador.
-        plt.plot(years, datosP_indicador)  # graficar los datos.
+        plt.plot(
+            datosP_indicador, label=f"{pais} - {descrP_indicador}"
+        )  # graficar los datos.
 
     plt.xlabel("Año")  # etiqueta del eje x.
     plt.ylabel("Valor")  # etiqueta del eje y.
